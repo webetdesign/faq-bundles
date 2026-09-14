0f673580af743dbe50220c140e14ebb9a55df97d
Alexis <alexis@webetdesign.com>
2026-09-14 16:26:38 +0200

    [4.x] Port the CMS integration to wd-cms 4.x namespaces

--- src/CMS/CategoryPage.php
+++ src/CMS/CategoryPage.php
@@ -4,11 +4,11 @@ namespace WebEtDesign\FaqBundle\CMS;
 
 use Symfony\Component\HttpFoundation\Request;
 use WebEtDesign\CmsBundle\Attribute\AsCmsPage;
-use WebEtDesign\CmsBundle\CmsBlock\TextBlock;
-use WebEtDesign\CmsBundle\CmsTemplate\AbstractPage;
-use WebEtDesign\CmsBundle\DependencyInjection\Models\BlockDefinition;
-use WebEtDesign\CmsBundle\DependencyInjection\Models\RouteAttributeDefinition;
-use WebEtDesign\CmsBundle\DependencyInjection\Models\RouteDefinition;
+use WebEtDesign\CmsBundle\CMS\Block\TextBlock;
+use WebEtDesign\CmsBundle\CMS\Template\AbstractPage;
+use WebEtDesign\CmsBundle\CMS\Configuration\BlockDefinition;
+use WebEtDesign\CmsBundle\CMS\Configuration\RouteAttributeDefinition;
+use WebEtDesign\CmsBundle\CMS\Configuration\RouteDefinition;
 use WebEtDesign\FaqBundle\Controller\CategoryController;
 use WebEtDesign\FaqBundle\Entity\Category;
 use WebEtDesign\FaqBundle\Entity\Faq;
